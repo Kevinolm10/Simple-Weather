a31f318bbd811aceb3cf022f6a4a604bcb028934
Kevin Ölmheden <kevinolmheden@MacBook-Air-som-tillhor-Kevin.local>
2025-08-10 14:57:35 +0200

    Install tkinter for GUI and set up a basic window where i can display the API data for the user

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import requests
-import json
-import os
-import datetime
-
+import tkinter as tk
+from tkinter import ttk
 
 API_KEY = "319411e71d70d6546b85d548e53e4c7c"
 city = "Stockholm"
 url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
+
+root = tk.Tk()
+root.title("Weather App")
+root.geometry("800x600")
+root.resizable(True, True)
+root.configure(bg="lightblue")
+
+root.mainloop()
 
 
 response = requests.get(url)
